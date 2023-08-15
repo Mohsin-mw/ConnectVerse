644672634a0dce565a1183ba320ca18ee2c33d5f
Mir Saheem Shafi <100963202+saheemshafi@users.noreply.github.com>
2023-08-15 23:40:51 +0530

    [Enh] : Add primaryColumn in Dashboard.tsx
    Designed the primary column layout. Header, Content Wrapper, Message Box.

---
+++ src/components/Dashboard/MainHeader.tsx
@@ -0,0 +1,49 @@
+import atIcon from "../../assets/icons/at.svg";
+import callIcon from "../../assets/icons/call.svg";
+import videoIcon from "../../assets/icons/video.svg";
+import pinIcon from "../../assets/icons/pin.svg";
+import addFriendIcon from "../../assets/icons/add-friend.svg";
+import inboxIcon from "../../assets/icons/inbox.svg";
+import helpIcon from "../../assets/icons/help.svg";
+import { Searchbar } from "..";
+
+/**
+ * MainHeader component represents the section containing various icons.
+ * The icons change based on whether a channel or user chat is selected.
+ */
+const MainHeader = () => (
+  <div className="w-full text-white py-4 px-4 h-16 border-b-raisin-black border-b-2 items-center flex justify-between">
+    
+    {/* User Profile Button : Will be used later to open profile modal */}
+    <button className="flex font-semibold gap-2 text-sm items-center">
+      <img src={atIcon} /> Emily{" "}
+      <div aria-hidden className="w-2 h-2 rounded-full bg-green"></div>
+    </button>
+
+    {/* Header Icons For Call, VideoCall, Pin, Add Friend, Inbox and Help */}
+    <div className="flex gap-6 [&>button]:shrink-0">
+      <button>
+        <img src={callIcon} />
+      </button>
+      <button>
+        <img src={videoIcon} />
+      </button>
+      <button>
+        <img src={pinIcon} />
+      </button>
+      <button>
+        <img src={addFriendIcon} />
+      </button>
+      <Searchbar />
+      <button>
+        <img src={inboxIcon} />
+      </button>
+      <button>
+        <img src={helpIcon} />
+      </button>
+    </div>
+
+  </div>
+);
+
+export default MainHeader;
--- src/components/Dashboard/PrimaryColumn.tsx
+++ src/components/Dashboard/PrimaryColumn.tsx
@@ -1,9 +1,21 @@
+import { MessageBox } from "..";
+import MainHeader from "./MainHeader";
+
 const PrimaryColumn = () => {
   return (
     <section
       aria-labelledby="primary-heading"
-      className="flex h-full min-w-0 flex-1 flex-col overflow-y-auto lg:order-last bg-red-500"
-    ></section>
+      className="flex relative h-full min-w-0 flex-1 flex-col overflow-y-auto lg:order-last bg-dark-charcoal"
+    >
+      {/* A static header that will update on route changes */}
+      <MainHeader />
+
+      {/* Main Content */}
+      <div className="flex-1 p-4 overflow-y-auto max-h-[calc(100vh_-_64px)"></div>
+
+      {/* Message box for handling messages, images, gifs, emojis */}
+      <MessageBox />
+    </section>
   );
 };
 
