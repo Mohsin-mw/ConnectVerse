a50d0da7aeac5fe9952b28d3296ba740c060fcda
Dominic DiModugno <anonym0uslemon@Dominics-Laptop.lan>
2023-08-17 18:02:24 -0500

    fixed TopBar.tsx

--- frontend/src/components/Friends/TopBar.tsx
+++ frontend/src/components/Friends/TopBar.tsx
@@ -12,21 +12,22 @@ import { Searchbar } from "..";
  *
  */
 const TopBar = () => (
-  <div className="w-full text-white py-4 px-4 h-16 border-b-raisin-black border-b-2 items-center flex gap-6">
+  <div className="w-full text-white py-4 px-4 h-16 border-b-raisin-black border-b-2 items-center justify-between flex gap-6">
     {/* User Profile Button : Will be used later to open profile modal */}
-    <button className="flex font-semibold gap-2 text-sm items-center">
-      <img src={atIcon} /> Friends
-    </button>
-
-    {/*  Buttons for online all pending blocked add friend */}
-    <div className="flex gap-6 [&>button]:shrink-0 [&>button]:p-[5px] [&>button]:rounded-md [&>button]:text-base">
-      <button className=" bg-[#202225]">Online</button>
-      <button className="">All</button>
-      <button className="">Pending</button>
-      <button className="">Blocked</button>
-      <button className="bg-[#248046] text-white">Add Friend</button>
+    <div className="flex">
+      <button className="flex font-semibold gap-2 text-sm items-center pr-5 border-r border-arsenic">
+        <img src={atIcon} /> Friends
+      </button>
+      {/*  Buttons for online all pending blocked add friend */}
+      <div className="flex gap-6 [&>button]:shrink-0 [&>button]:p-[5px] [&>button]:rounded-md [&>button]:text-base">
+        <button className=" bg-[#202225] ml-5">Online</button>
+        <button className="">All</button>
+        <button className="">Pending</button>
+        <button className="">Blocked</button>
+        <button className="bg-[#248046] text-white">Add Friend</button>
+      </div>
     </div>
-    <div className="flex gap-6 ">
+    <div className="flex gap-6">
       <button>
         <img src={callIcon} />
       </button>
